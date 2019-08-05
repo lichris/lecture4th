2597a09e8c601de45e96585226b567d208751e47
Heeseung Lee <final.lee@thecorporation.co.kr>
2019-08-05 14:39:57 +0900

    auth 미들웨어 추가

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -36,37 +36,3 @@ export default async (req, res, next) => {
     next(err)
   }
 }
-
-
-
-
-
-/*
-req.user = null
-
-    if (req.headers.authorization) {
-      let uuid
-      jwt.verify(
-        req.headers.authorization.split(' ')[1],
-        process.env.JWT_SECRET,
-        (err, payload) => {
-          if (err) {
-            return next(createError(401, '토큰 정보가 유효하지 않습니다.'))
-          }
-
-          uuid = payload.uuid
-        }
-      )
-
-      const userRepo = new UserRepo()
-      const user = await userRepo.find(uuid)
-
-      if (!user) {
-        return next(createError(404, '사용자를 찾을 수 없습니다.'))
-      }
-
-      req.user = user
-    }
-
-    next()
-*/
